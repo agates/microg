39ba204eec886d03d725fd13f13f039998ccc3dd
Hammergrat7 <45185502+Hammergrat7@users.noreply.github.com>
2018-12-05 03:18:44 +0000

    Update 90004-modify-build-fingerprint.sh

--- 90004-modify-build-fingerprint.sh
+++ 90004-modify-build-fingerprint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-GOOGLE_FINGERPRINT=$(echo awk -F"=" '/ro.bootimage.build.fingerprint/{print $2}' ${BUILD_DIR}/vendor/android-prepare-vendor/${DEVICE}/$(tr '[:upper:]' '[:lower:]' <<< "${AOSP_BUILD}")/factory_imgs_data/system/default.prop)
+RELEASE_KEYS_CUSTOM=$(awk -F"=" '/ro.bootimage.build.fingerprint/{print $2}' ${BUILD_DIR}/vendor/android-prepare-vendor/${DEVICE}/$(tr '[:upper:]' '[:lower:]' <<< "${AOSP_BUILD}")/factory_imgs_data/system/default.prop)
 
-sed -i 's@$(PRODUCT_BRAND)/$(TARGET_PRODUCT)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BF_BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)@${GOOGLE_FINGERPRINT}@' ${BUILD_DIR}/build/core/Makefile
-
+sed -i s@$(PRODUCT_BRAND)/$(TARGET_PRODUCT)/$(TARGET_DEVICE):$(PLATFORM_VERSION)/$(BUILD_ID)/$(BF_BUILD_NUMBER):$(TARGET_BUILD_VARIANT)/$(BUILD_VERSION_TAGS)@${RELEASE_KEYS_CUSTOM}@ ${BUILD_DIR}/build/core/Makefile
